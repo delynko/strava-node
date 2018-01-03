a2895e1080053a9217208e0e32f12cdb26885254
delynko <deynko@gmail.com>
2018-01-03 14:57:18 -0700

    changed date format

--- routes/strava.js
+++ routes/strava.js
@@ -13,27 +13,30 @@ for (var i = 0; i < pages.length; i++){
         if (payload != undefined){
             if (!err) {
                 for (var x = 0; x < payload.length; x++){
-//                    if (payload[x].type != "Walk"){
                         
-                        var date = new Date(payload[x].start_date_local);
-                        var year = date.getFullYear();
-                        var month = date.getMonth();
-                        var day = date.getDay();
-                        
-                        var activity = {
-                            id: payload[x].id,
-                            type : payload[x].type,
-                            line : payload[x].map.summary_polyline,
-                            name: payload[x].name,
-                            elevation_gain: payload[x].total_elevation_gain,
-                            distance: (payload[x].distance * 0.000621371),
-                            year: year,
-                            month: month,
-                            day: day,
-                            date: date
-                        }
-                        activities.push(activity);
-//                    }
+                    var date = new Date(payload[x].start_date_local);
+                    var year = date.getFullYear();
+                    var month = date.getMonth();
+                    var day = date.getDay();
+                    
+                    var dateArray = date.toDateString().split(" ");
+                    
+                    dateArray.pop()
+                    
+                    var activity = {
+                        id: payload[x].id,
+                        type : payload[x].type,
+                        line : payload[x].map.summary_polyline,
+                        name: payload[x].name,
+                        elevation_gain: payload[x].total_elevation_gain,
+                        distance: (payload[x].distance * 0.000621371),
+                        year: year,
+                        month: month + 1,
+                        day: day + 1,
+                        date: dateArray.join(" ")
+                    }
+                    activities.push(activity);
+                    
                 }
             } else {
                 console.log(err);
